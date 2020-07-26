ddf89cd302374b64cc31ef4ab62a3664daf8957d
Sana Mohammad Rafiq <63970264+Sana-Mohammad-Rafiq25@users.noreply.github.com>
2020-07-26 08:52:25 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let enterBudget = 0;
 let total = 0;
-let Balance = enterBudget - total;
+let Balance = 0;
+let quantity = 0;
+//let Balance = enterBudget - total;
 let quantityOfApple = 0;
 const priceOfApple = 28;
 let quantityOfBanana = 0;
@@ -10,7 +12,7 @@ const priceOfMango = 25;
 let totalCostOfApple = 0;
 let totalCostOfMango = 0;
 let totalCostOfBanana = 0;
-let quantity = 0;
+let totalQuantity = 0;
 
 
 
@@ -23,73 +25,77 @@ function inputBudget() {
 
     let enterBudget = parseInt(document.querySelector("#enter-budget").value);
     console.log(enterBudget);
-    console.log(typeof enterBudget);
+
     let budgetValue = document.querySelector("#budget-value");
     budgetValue.innerHTML = "Your budget is:" + enterBudget;
-    // remainingBalance = inputBudget - consumeMoney;
+    // Balance = enterBudget - total;
 
+    if (enterBudget <= 0) {
+        alert("first enter your budget!");
+    } else if (enterBudget <= 5) {
 
-    if (enterBudget <= 5) {
-        
         alert("Your Budget is not sufficient!");
-        //console.log("your budget is not sufficient")
-        alert("first enter your budget!"); 
+
     }
-    // else{
-    // switch enterBudget()
-    // }
 
-
+    // switch enterBudget() {}
 }
 
-function checkQuantity() {
-    //budget = document.getElementById("enterBudget").Value;
-    //  return quantityOfApple, quantityOfBanana, quantityOfMango <= 0;
 
-    quantity = quantityOfApple + quantityOfMango + quantityOfBanana;
-    document.querySelector("#quantity").innerHTML = quantity;
-    console.log(quantity);
-}
+
+
 
 function calculateBalance() {
-    budget = document.getElementById("enter-budget").value;
-    remainingBalance = enterBudget - total;
-    document.getElementById("remainingBalance").innerHTML = Balance;
+    document.getElementById("enter-budget").value = enterBudget;
+    document.getElementById("remaining-balance").innerHTML = Balance;
+    document.getElementById("total").innerHTML = total;
+    Balance = enterBudget - total;
 
     console.log("remainingBalance");
+    inputBudget();
+    checkTotal();
 }
 
-// function inputBudget() {
-//     let price = document.querySelector("input").value;
-//     console.log(price);
-//     console.log(typeOf );
-//     // document.getElementById("demo").innerHTML = x;
-//   }
 
 function checkTotal() {
     total = totalCostOfApple + totalCostOfMango + totalCostOfBanana;
     document.querySelector("#total").innerHTML = total;
     console.log(total);
-    //if (total > enterBudget) {
-    //alert("insufficient budget");
-    //     console.log("insufficient budget");
-    // }
+    if (total > enterBudget) {
+        alert("insufficient budget");
+        //     console.log("insufficient budget");
+    }
     // else {
     //     console.log("you have sufficient money");
     // }
 
 }
 
+function checkQuantity() {
+    document.querySelector("#quantityOfApple").innerHTML = quantityOfApple;
+    document.querySelector("#quantityOfMango").innerHTML = quantityOfMango;
+    document.querySelector("#quantityOfBanana").innerHTML = quantityOfBanana;
+
+    quantity = quantityOfApple + quantityOfMango + quantityOfBanana;
+    document.querySelector("#quantity").innerHTML = quantity;
+
+    console.log(quantity);
+
+}
+
 function calculateTotalOfApple() {
     totalCostOfApple = quantityOfApple * priceOfApple;
     document.getElementById("total").innerHTML = total;
-    checkTotal()
+    checkTotal();
+    checkQuantity();
+    calculateBalance()
 }
 
 function addApple() {
     ++quantityOfApple;
     document.getElementById("quantityOfApple").innerHTML = quantityOfApple;
 
+    inputBudget()
     calculateTotalOfApple();
     calculateBalance();
 
@@ -103,6 +109,7 @@ function minusApple() {
     // alert("fruit is already zero");}
     calculateTotalOfApple();
     calculateBalance();
+
 }
 
 
@@ -118,13 +125,17 @@ function checkResult() {
 function calculateTotalOfMango() {
     totalCostOfMango = quantityOfMango * priceOfMango;
     document.getElementById("total").innerHTML = total;
-    checkTotal()
+    checkTotal();
+    checkQuantity();
+    calculateBalance();
+
+
 }
 
 function addMango() {
     ++quantityOfMango;
     document.getElementById("quantityOfMango").innerHTML = quantityOfMango;
-
+    inputBudget()
     calculateTotalOfMango();
     calculateBalance();
 }
@@ -133,6 +144,7 @@ function minusMango() {
     if (quantityOfMango > 0) --quantityOfMango;
     document.getElementById("quantityOfMango").innerHTML = quantityOfMango;
 
+    inputBudget()
     calculateTotalOfMango();
     calculateBalance();
 }
@@ -141,13 +153,16 @@ function minusMango() {
 function calculateTotalOfBanana() {
     totalCostOfBanana = quantityOfBanana * priceOfBanana;
     document.getElementById("total").innerHTML = total;
-    checkTotal()
+    checkTotal();
+    checkQuantity();
+    calculateBalance();
+    inputBudget();
 }
 
 function addBanana() {
     ++quantityOfBanana;
     document.getElementById("quantityOfBanana").innerHTML = quantityOfBanana;
-
+    inputBudget()
     calculateTotalOfBanana();
     calculateBalance();
 }
@@ -155,7 +170,7 @@ function addBanana() {
 function minusBanana() {
     if (quantityOfBanana > 0) --quantityOfBanana;
     document.getElementById("quantityOfBanana").innerHTML = quantityOfBanana;
-
+    inputBudget()
     calculateTotalOfBanana();
     calculateBalance();
 }
