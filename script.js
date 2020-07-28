bd023542c8de7a4d58c0961234b37391aaf93b98
Sana Mohammad Rafiq <63970264+Sana-Mohammad-Rafiq25@users.noreply.github.com>
2020-07-28 02:28:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ let totalCostOfMango = 0;
 let totalCostOfBanana = 0;
 let totalQuantity = 0;
 
-
+// acha chalo yeh mein dekhti khud code files download karke phir batati baki wo
+// let wala samjh agaya ?
+// ma 'am thora to agaya or abhi read krngi code phir Inshallah ajayega warna jab pora ban jay ga na tou mein dobara seysab explain kardongi
+// ok theek jaiur dont worry aapka late submission count nai hoga: ) Thank you so much ma 'am and sorry woh group mai thori behas ho gaejee avoid kia akrey behas we are all here to learn <3 Allah Hafiz Allah
 
 // function onload() {
 //     console.log("---", document.getElementById("total"));
@@ -23,18 +26,18 @@ let totalQuantity = 0;
 
 function inputBudget() {
 
-    let enterBudget = parseInt(document.querySelector("#enter-budget").value);
-    console.log(enterBudget);
+    enterBudget = parseInt(document.querySelector("#enter-budget").value);
+
+    console.log('enterbudget' + enterBudget);
 
     let budgetValue = document.querySelector("#budget-value");
+    console.log('budgetValue' + budgetValue)
     budgetValue.innerHTML = "Your budget is:" + enterBudget;
     // Balance = enterBudget - total;
 
-    // if (enterBudget <= 0) {
-    //     alert("first enter your budget!");
-    // } else 
-    if (enterBudget <= 5) {
-
+    if (enterBudget <= 0) {
+        alert("first enter your budget!");
+    } else {
         alert("Your Budget is not sufficient!");
 
     }
@@ -47,7 +50,7 @@ function inputBudget() {
 
 
 function calculateBalance() {
-    document.getElementById("enter-budget").value = enterBudget;
+    // document.getElementById("enter-budget").value = enterBudget;
     document.getElementById("remaining-balance").innerHTML = Balance;
     document.getElementById("total").innerHTML = total;
     Balance = enterBudget - total;
@@ -57,21 +60,17 @@ function calculateBalance() {
     checkTotal()
 }
 
-
+//Aur yeh wala bhi?
 function checkTotal() {
     total = totalCostOfApple + totalCostOfMango + totalCostOfBanana;
     document.querySelector("#total").innerHTML = total;
     console.log(total);
-    if (total > enterBudget) {
-        alert("insufficient budget");
-        //     console.log("insufficient budget");
-    }
     // else {
     //     console.log("you have sufficient money");
     // }
 
 }
-
+// yeh function kidher call karwaya hai? But kahin call bhi tou karwain hogey balkay choro wait
 function checkQuantity() {
     document.querySelector("#quantityOfApple").innerHTML = quantityOfApple;
     document.querySelector("#quantityOfMango").innerHTML = quantityOfMango;
